f867abc35874a3192f3f068a73fe24c8a20d3a5e
Swellington Soares <swellington.santos@gmail.com>
2026-06-28 02:48:34 -0300

    Projeto inicial.

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("io.freefair.lombok") version "9.5.0"
+}
+
+group = "dev.swell"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
