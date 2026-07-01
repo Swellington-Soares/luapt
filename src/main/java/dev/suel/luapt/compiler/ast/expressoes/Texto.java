cc5fbeab5ca0fd88e83f5ad46f59525427f54960
Swellington Soares <swellington.santos@gmail.com>
2026-07-01 19:07:30 -0300

    Correção nos char de quebra de linha.

--- src/main/java/dev/suel/luapt/compiler/ast/expressoes/Texto.java
+++ src/main/java/dev/suel/luapt/compiler/ast/expressoes/Texto.java
@@ -4,8 +4,24 @@ import dev.suel.luapt.compiler.ast.No;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
-@Getter
 public class Texto extends No {
     public final String valor;
+
+    private static Map<String, Character> SIMBOLO_CONTROL = Map.of(
+           "\\n", '\n',
+           "\\r", '\r',
+           "\\t", '\t',
+           "\\0", '\0'
+    );
+
+    public String getValor() {
+        var texto = valor;
+        for (Map.Entry<String, Character> entry : SIMBOLO_CONTROL.entrySet()) {
+            texto = texto.replace(entry.getKey(), entry.getValue().toString());
+        }
+        return texto;
+    }
 }
